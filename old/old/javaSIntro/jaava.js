aa02093013c5aeb0811576e0df9daa93a7cac66d
Alivar <ethanhanson9@gmail.com>
2023-09-18 14:55:13 -0700

    added all my old and current assignments

---
+++ old/old/javaSIntro/jaava.js
@@ -0,0 +1,16 @@
+/**
+ * open the webpage
+ * display alert to user
+ * diplay 3 numbers that were genterated with 3 differect math tags
+ * 
+ */
+
+// generate the first number
+var num1 = 5*13;
+//generate the second number
+var num2 = 96/4;
+//generate the thrid number
+var num3 = 11+9;
+
+// Display a message to the user do this by writing sting and then appending in the variables to display their value
+alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination:\n" + "Combination: " + num1 + " - " + num2 + " - " + num3);
