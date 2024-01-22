ff6f57955b71e596fa6a5d6160cd72574b6159ab
Alivar <ethanhanson9@gmail.com>
2024-01-21 19:12:23 -0800

    set up the use state. From what I understand I inported my array into react and then made it a constant state similar to when we were calling from the api

--- 1.18.24/Puppy Pals/Puppy Pals/src/App.jsx
+++ 1.18.24/Puppy Pals/Puppy Pals/src/App.jsx
@@ -1,15 +1,24 @@
+import { puppyList } from './data.js'
+import { useState } from 'react'
 import './App.css'
 
 
 //deleted all the old counter code and imports
-
-
+//imported puppy list from the new data file i made
+//
 function App() {
+
+  const [puppies, setPuppies] = useState(puppyList);// what i am pretty sure happened was i have made it so the puppy list array is now a constant state that I can now use through react across my whole project.
+  console.log(puppies);
 
   return (
     <>
-      <div>
-       
+      <div className="App">
+      { 
+   puppies.map((puppy) => {
+     return <p key={puppy.id}>{puppy.name}</p>//basically generated the list onto my html page via js. Also generated each instances ids.
+   })
+}
       </div>
     </>
   )
